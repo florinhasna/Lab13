8e1394fab31f2564def5c63ba0e7f0fed9f9270b
Florin Hasna <florinhasna@outlook.com>
2024-01-26 10:48:23 +0000

    Added comparation method

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "DateADT.h"
 using namespace std;
 
+bool operator<(const Date& lhs, const Date& rhs);
 
 int main() {
     Date now = Date(26, 1, 2024);
@@ -9,4 +10,18 @@ int main() {
 
     cout << now.get_date() << endl;
     cout << tomorrow.get_date() << endl;
+}
+
+bool operator<(const Date& lhs, const Date& rhs) {
+    if (year < rhs.get_year()){
+        return true;
+    } else if (lhs.get_year() == rhs.get_year() &&
+               lhs.get_month() < rhs.get_month()) {
+        return true;
+    } else if (lhs.get_year() == rhs.get_year() &&
+               lhs.get_month() == rhs.get_month() &&
+               lhs.get_day() < rhs.get_day()) {
+        return true;
+    }
+    return false;
 }
