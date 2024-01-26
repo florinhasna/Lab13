b9c8a1afc91beac5cad34fe1737c6fb76e1bd204
Florin Hasna <florinhasna@outlook.com>
2024-01-26 10:29:37 +0000

    Source code file for class Date

---
+++ DateADT.cpp
@@ -0,0 +1,17 @@
+Date::Date(int day, int month, int year) {
+    this->day = day;
+    this->month = month;
+    this->year = year;
+}
+
+int Date::get_day() {
+    return day;
+}
+
+int Date::get_month() {
+    return month;
+}
+
+int Date::get_year() {
+    return year;
+}
